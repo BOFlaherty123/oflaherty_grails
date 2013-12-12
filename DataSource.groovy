af035d5d9f78b9b164fc246dfd50943832388963
Benjamin OFlaherty <ben.o'flaherty@ageas.co.uk>
2013-12-12 17:31:21 +0000

    Create DataSource.groovy
    DataSource.groovy

---
+++ DataSource.groovy
@@ -0,0 +1,36 @@
+dataSource {
+    pooled = true
+    driverClassName = "com.mysql.jdbc.Driver"
+    dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+}
+hibernate {
+    cache.use_second_level_cache = true
+    cache.use_query_cache = false
+    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+	show_sql=true
+}
+// environment specific settings
+environments {
+    development {
+        dataSource {
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost/fileupload?useUnicode=yes&characterEncoding=UTF-8"
+            username = // hidden
+            password = // hidden
+        }
+    }
+    test {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+        }
+    }
+    production {
+        dataSource {
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost/fileupload?useUnicode=yes&characterEncoding=UTF-8"
+            username = // hidden
+            password = // hidden
+        }
+    }
+}
